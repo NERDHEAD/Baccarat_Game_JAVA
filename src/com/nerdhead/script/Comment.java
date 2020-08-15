2f90426553af4bece7f9c1ad39af306aa7bad7d3
Nerdhead <wntjd337@gmail.com>
2020-08-15 18:50:47 +0900

    Create Class : Comment

---
+++ src/com/nerdhead/script/Comment.java
@@ -0,0 +1,24 @@
+package com.nerdhead.script;
+
+ 
+
+public class Comment implements DealerComment{
+
+	private void dealerSay(String str) {
+
+		System.out.println(str);
+
+	}
+
+	private void systemSay(String str) {
+
+		System.out.println(str);
+
+	}
+
+	
+	
+	
+
+	
+}
