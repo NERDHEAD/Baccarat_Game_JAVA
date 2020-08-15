9ef944054f08f112d3eeb3089affa328af300d00
Nerdhead <wntjd337@gmail.com>
2020-08-15 20:30:07 +0900

    Create Interface : Result -> for CONSTANT

---
+++ src/com/nerdhead/rull/Result.java
@@ -0,0 +1,11 @@
+package com.nerdhead.rull;
+
+
+/**
+ * 승 패 무승부 상수로 쓰기 위한 interface;
+ */
+public interface Result{
+	public static final	int PLAYER_WIN=1;
+	public static final	int BANKER_WIN=-1;
+	public static final	int TIE=0;
+}
