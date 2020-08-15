2d4d1837c405b29c0e85f9863957611ea07becc1
Nerdhead <wntjd337@gmail.com>
2020-08-15 18:51:07 +0900

    Create Interface : DealerComment

---
+++ src/com/nerdhead/script/DealerComment.java
@@ -0,0 +1,28 @@
+package com.nerdhead.script;
+
+ 
+
+public interface DealerComment {
+	/**
+	 * 시작
+	 */
+	public static final String WELCOME="환영...";
+	
+	/**
+	 * 손님 접대
+	 */
+	public static final String ASK_GUESTS_COUNT="몇명?";
+	public static final String ASK_GUESTS_NAME="이름?";
+	
+	public static final String BET_PLZ="배팅";
+	public static final String BET_END="그만";
+	
+	public static final String TAKE_CARD="자, 카드";
+	public static final String CHECK_RULL="";
+	
+	
+//	public static final String	 
+	
+	
+	
+}
