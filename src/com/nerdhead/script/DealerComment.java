24e3294162435d531db50c9847894a83e8789d7d
Nerdhead <wntjd337@gmail.com>
2020-08-15 20:28:48 +0900

    updated : DealerComment -> .

--- src/com/nerdhead/script/DealerComment.java
+++ src/com/nerdhead/script/DealerComment.java
@@ -1,7 +1,5 @@
 package com.nerdhead.script;
 
- 
-
 public interface DealerComment {
 	/**
 	 * 시작
@@ -11,14 +9,17 @@ public interface DealerComment {
 	/**
 	 * 손님 접대
 	 */
-	public static final String ASK_GUESTS_COUNT="몇명?";
+	public static final String ASK_GUESTS_COUNT="몇명";
 	public static final String ASK_GUESTS_NAME="이름?";
 	
 	public static final String BET_PLZ="배팅";
 	public static final String BET_END="그만";
 	
 	public static final String TAKE_CARD="자, 카드";
-	public static final String CHECK_RULL="";
+	public static final String CHECK_RULL="( 생각중... )";
+	public static final String GAME_RESULT="%s의 %s";		//(Player/Banker)의 승리! or 무승부! 로 뜨게
+	
+	
 	
 	
 //	public static final String	 
