16f3feae92977c7294fa373ef60050f6c8c726c3
Nerdhead <wntjd337@gmail.com>
2020-08-17 05:37:51 +0900

    Update : Result_DTO -> add GETTER/SETTER more

--- src/com/nerdhead/rull/Result_DTO.java
+++ src/com/nerdhead/rull/Result_DTO.java
@@ -1,4 +1,10 @@
 package com.nerdhead.rull;
+
+import java.util.List;
+
+import com.nerdhead.card.CardOne;
+import com.nerdhead.input_output_manager.DealerComment;
+
 /**
  * @since 2020.08.15
  * @author nerdhead
@@ -11,6 +17,11 @@ public class Result_DTO implements Result{
 	private boolean lucky6_1=false;
 	private boolean lucky6_2=false;
 	
+	private List<CardOne> player;
+	private int pScore;
+	private List<CardOne> banker;
+	private int bScore;
+	
 	//각각의 값이 따로 판정되서 set 되므로 생성자에서 한번에 결정될 일이 아님
 //	public Result_DTO(int result,boolean player_pair,boolean banker_pair,
 //			boolean lucky6_1,boolean lucky6_2) {
@@ -20,7 +31,8 @@ public class Result_DTO implements Result{
 //		this.lucky6_1=lucky6_1;
 //		this.lucky6_2=lucky6_2;
 //	}
-	
+
+
 	
 	/**GETTER*/
 	public int getResult() {
@@ -38,6 +50,21 @@ public class Result_DTO implements Result{
 	public boolean isLucky6_2() {
 		return lucky6_2;
 	}
+	public int getPScore() {
+		return pScore;
+	}
+	public int getBScore() {
+		return bScore;
+	}
+	
+	//판 끝날때 결과 출력용으로 밖에 안쓸듯
+//	public List<CardOne> getPlayer() {
+//		return player;
+//	}
+//	public List<CardOne> getBanker() {
+//		return banker;
+//	}
+	
 	
 	/**SETTER*/
 	public void setResult(int result) {
@@ -55,5 +82,48 @@ public class Result_DTO implements Result{
 	public void setLucky6_2(boolean lucky6_2) {
 		this.lucky6_2 = lucky6_2;
 	}
+	public void setPlayer(List<CardOne> player) {
+		this.player = player;
+	}
+	public void setBanker(List<CardOne> banker) {
+		this.banker = banker;
+	}
+	public void setPScore(int cardSum) {
+		pScore=cardSum;		
+	}
+	public void setBScore(int cardSum) {
+		bScore=cardSum;				
+	}
+	
+	
+	/******Override******/
+	/**
+	 * Player가 승리하면 P, Banker가 승리하면 B, 무승부는 T를 돌려줌
+	 */
+	@Override
+	public String toString() {
+		switch (result) {
+		case Result.PLAYER_WIN: 	return "P";
+		case Result.BANKER_WIN:	return "B";
+		case Result.TIE:					return "T";
+		default:							return "?";
+		}
+	}
+	public String getCardOfResult() {
+		StringBuilder str=new StringBuilder();
+		
+		str.append(DealerComment.PLAYER).append(" : ");
+		for (CardOne c : player) {
+			str.append(String.format("%-8s", c.toString()));
+		}
+		str.append("\n");
+		str.append(DealerComment.BANKER).append(" : ");
+		for (CardOne c : banker) {
+			str.append(String.format("%-8s", c.toString()));
+		}
+		
+		return str.toString();
+	}
+
 	
 }
