8be8998bba772b8939a12ba3f858172a35a18836
Nerdhead <wntjd337@gmail.com>
2020-08-16 15:21:09 +0900

    Update :  Baccarat_Game -> add abstract method

--- src/com/nerdhead/baccaratgame/Baccarat_Game.java
+++ src/com/nerdhead/baccaratgame/Baccarat_Game.java
@@ -1,11 +1,26 @@
 package com.nerdhead.baccaratgame;
 
 import com.nerdhead.input_output_manager.Comment;
-
-public class Baccarat_Game {
-	/*Member Variable*/
-	Comment print=new Comment();
+/**
+ * @since 2020.08.16
+ * @author nerdhead
+ * Manager는 단순히 게임 로직 + 결과(Result_DTO)의 LinkedList만 가지고있음(결과는 순서대로 누적되어야 하기 떄문)
+ * Manager를 상속받아 손님의 기능을 추가함
+ */
+public class Baccarat_Game extends Baccarat_Game_Manager{	//manager는 게임 시작후 단순히 진행, 게임 결과만 가지는 class
+	private final static int GIVE_UP=-1;
+	private final static int RETRY=0;
+	private final static int CONTINUE=1;
 	
+	
+	
+	
+	/*Member Variable*/
+	private Comment print=new Comment();		//출력 담당 일진 클래스	-> 원하는 대사가 알차게 들어있음
+	private AskAbout ask=new AskAbout();			//입력 담당 일진 클래스	-> 요구한 값이 맞는지 전부 검증해줌
+	private Guests guests;								//손님 담당 일진 클래스	-> 손님수, 손님칩수, 턴수를 정해줘야함	-> 안하면 화냄
+	
+
 	
 	/**
 	 * constructor
@@ -18,8 +33,10 @@ public class Baccarat_Game {
 	 * initialize
 	 */
 	private void init() {
-		guest.setChip(Guest.Default);
-		guest.set
+		/*게스트 초기화*/
+		guests=new Guests();
+		guests.setChip(Guests.DefaultChip);
+		guests.setMaxTurn(Guests.DefaultTurn);
 	}
 	
 	
@@ -27,9 +44,6 @@ public class Baccarat_Game {
 	/**
 	 * 게임 시작 Method
 	 */
-	public void 왜안되는거야() {
-		System.out.println("왜안되는거야");
-	}
 
 	public void start() {
 		/************************************************* 기본 흐름 ****************************************************
@@ -47,24 +61,54 @@ public class Baccarat_Game {
 		 * 			c. 손님 수변경 + 칩과 판수 재설정 		-> 처음부터
 		 * 			d. 게임 종료 								-> break;
 		 *************************************************************************************************************/
+		while(true) {
+			if(guests.isFirst()) {guestsSetting();}												//1. 손님수, 손님이름 둘다 체크 후 없으면 받기
+			guestsBetting();																		//2. 게스트 배팅하기
+			super.start();		//SuperClass														//3. 딜러가 게임을 진행함 -> Result_DTO(protected)에 결과가 저장됨
+			gameResult();																			//4. 배팅 정산 및 결과 출력
+																										//계속할껀지 상태보고 정하기 
+			switch (askContinue()) {
+			case value:
+				
+				break;
+
+			default:
+				break;
+			}
+			
+			
+			
+		}
 		
 		
+		
+		
+
+		
+		
+	}
+	private void guestsSetting() {
 		print.dealerSay_ASK_GUESTS_COUNT();			//몇명이서 온지 질문 출력
-		/*
-		 * ask는 입력만 받는 Class -> 규칙에 맞는 값만 받을수있게함  
-		 * 		method()는 실수 했을 때 default 값으로 다시해달라고 알려줌, 
-		 * 		method(String error_massage)는 실수 했을 때 error_massage로 안내함 
-		 * */
-		guest.setCount(ask.checkGeustCount());
-//		guest.setChip(1000);									//받지 않으면 default 값으로 init에서 처리함
+		guests.setCount(ask.checkGeustCount());
+//		guests.setChip(1000);									//받지 않으면 default 값으로 init에서 처리함
+//		guests.setMaxTurn(20);								//받지 않으면 default 값으로 init에서 처리함
 		
+		print.dealerSay_ASK_GUESTS_NAME();
+		guests.setName(ask.checkGuestName(guests.getGuestInfo()));
+	}
+	
+	private void guestsBetting() {
+		print.dealerSay_BET_PLZ();
+		guests.betChips(ask.checkGuestBet(guests.getGuestInfo()));			//손님들의 dto array받아서 순차 질문 후 해당 질문을 다시 dto에 담아서 return함
+		print.dealerSay_BET_END();
+	}
+	
+	private void gameResult() {
+		print.dealerSay_GAME_RESULT(gameResult);							//게임 결과 dto를 통해 결과를 딜러가 출력
 		
-		
-		
-		
-		
-		
-		
-		
+		guests.setGameResult(gameResult);										//결과 입력 
+		print.systemSay_GEUSTS_RESULT(guests.getGameResult());			//결과 출력	(모든 손님의 배팅결과가 출력됨)		//파산 포함
+		//ex)  "누구누구"님이 칩 800개 Luck6!! 18배율!	 등등 출력후 정산표 출력
+		print.systemSay_HISTORY_BOARD(gameResultList);					//게임 누적 결과를 받아서 출력해줌
 	}
 }
