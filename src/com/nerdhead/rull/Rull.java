1f96964be53254bf149a1dba0b75eaeef6b70b44
GDJ27 <GDJ27@DESKTOP-U2LMTEP>
2020-08-18 18:34:53 +0900

    Update : Rull -> add comment

--- src/com/nerdhead/rull/Rull.java
+++ src/com/nerdhead/rull/Rull.java
@@ -19,7 +19,7 @@ public class Rull {
 			{true,	true,	true,	true,	false,	false,	false,	false,	true,	true},		//뱅커 합 : 5
 			{true,	true,	true,	true,	true,	true,	false,	false,	true,	true},		//뱅커 합 : 6
 			{true,	true,	true,	true,	true,	true,	true,	true,	true,	true},		//뱅커 합 : 7
-	};		//0		1		2		3		4		5		6		7		8		9
+	};		//0		1		2		3		4		5		6		7		8		9		//player의 3번째 카드의 점수
 	
 	
 	
